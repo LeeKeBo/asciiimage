e8e57b6062c23707dd8c0a8363d2c726687a27ac
LeeKeBo <42863445+LeeKeBo@users.noreply.github.com>
2020-02-12 01:11:22 +0800

    add gif Converter

---
+++ src/main/java/com/asciiimage/asciiimage/process/asciiConvert/gifConvert.java
@@ -0,0 +1,40 @@
+package com.asciiimage.asciiimage.process.asciiConvert;
+
+import com.asciiimage.asciiimage.process.charFitStrategy.charFitStrategy;
+
+import com.asciiimage.asciiimage.process.utils.*;
+
+/**
+ * gif的转换可以通过继承之前的图片转换器来实现，只需将GIF逐帧读取后，对每一帧都进行一次变化后再存入即可
+ */
+public class gifConvert extends AsciiToImageConvert{
+
+    public gifConvert(AsciiCache asciiCache, charFitStrategy charFitStrategy) {
+        super(asciiCache, charFitStrategy);
+    }
+
+    public int gitConverter(String srcPath,String targetPath,final int delay,final int repeat){
+        GifDecoder gifDecoder = new GifDecoder();
+        int status = gifDecoder.read(srcPath);
+        if(status == 0){
+            AnimatedGifEncoder animatedGifEncoder = new AnimatedGifEncoder();
+            boolean targetstatus = animatedGifEncoder.start(targetPath);
+            if(targetstatus){
+                animatedGifEncoder.setDelay(delay);
+                animatedGifEncoder.setRepeat(repeat);
+                int count = gifDecoder.getFrameCount();
+                for(int i=0;i<count;i++){
+                    animatedGifEncoder.addFrame(convertImage(gifDecoder.getFrame(i)));
+                }
+                animatedGifEncoder.finish();
+                return 1;       // 成功转换
+            }
+            else{
+                return 0;       // 目标文件路径有误
+            }
+        }
+        else{
+            return -1;          // 源文件路径有误
+        }
+    }
+}
--- src/main/java/com/asciiimage/asciiimage/process/test.java
+++ src/main/java/com/asciiimage/asciiimage/process/test.java
@@ -11,15 +11,17 @@ import java.io.IOException;
 
 public class test {
     public static void main(String[] args) throws IOException {
-            Font font = new Font("Courier", Font.BOLD, 6);
-            AsciiCache asciiCache = new AsciiCache(font,new char[] {'\\','|', ' ', '/','-'});
+            Font font = new Font("Courier", Font.BOLD, 3);
+            AsciiCache asciiCache = new AsciiCache(font);
             charFitStrategy charFitStrategy = new ssimFitStrategy();
-            ImageConvert convert = new AsciiToImageConvert(asciiCache, charFitStrategy);
-            BufferedImage source = ImageIO.read(new File(
-                    "examples/portrait.png"));
-            BufferedImage result = (BufferedImage) convert.convertImage(source);
-            ImageIO.write(result, "png",
-                    new File("examples/portrait_small_square_error.png"));
+//            ImageConvert convert = new AsciiToImageConvert(asciiCache, charFitStrategy);
+//            BufferedImage source = ImageIO.read(new File(
+//                    "examples/portrait.png"));
+//            BufferedImage result = (BufferedImage) convert.convertImage(source);
+//            ImageIO.write(result, "png",
+//                    new File("examples/portrait_small_square_error.png"));
+            gifConvert gifConverterTest = new gifConvert(asciiCache,charFitStrategy);
+            gifConverterTest.gitConverter("examples/test.gif","examples/test-result.gif",10,0);
 
 
     }
